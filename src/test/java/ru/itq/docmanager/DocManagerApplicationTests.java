e62d5b79a0f4621107cb32fcf0ba598f9f34acac
artur <devmagomedov@gmail.com>
2026-02-01 16:32:23 +0300

    First commit

---
+++ src/test/java/ru/itq/docmanager/DocManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itq.docmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
